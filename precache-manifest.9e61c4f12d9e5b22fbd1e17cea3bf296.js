cc63e3cb61fa9767b0a466468b60b56f39706776
Igor Bolotnikov <igorbolotnikov1993@gmail.com>
2020-02-20 16:42:22 +0200

    Updates

---
+++ precache-manifest.9e61c4f12d9e5b22fbd1e17cea3bf296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a798e6b2494c3abe9bd26fb392c976a",
+    "url": "/Coin-change-visualizer/index.html"
+  },
+  {
+    "revision": "af3643676ff007d8d9c3",
+    "url": "/Coin-change-visualizer/static/css/2.3d56841b.chunk.css"
+  },
+  {
+    "revision": "59fcc86c6ee5eafc70ae",
+    "url": "/Coin-change-visualizer/static/css/main.799089a9.chunk.css"
+  },
+  {
+    "revision": "af3643676ff007d8d9c3",
+    "url": "/Coin-change-visualizer/static/js/2.bf22e14b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Coin-change-visualizer/static/js/2.bf22e14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fcc86c6ee5eafc70ae",
+    "url": "/Coin-change-visualizer/static/js/main.496a0183.chunk.js"
+  },
+  {
+    "revision": "ae0a80fd08f16ce33a56",
+    "url": "/Coin-change-visualizer/static/js/runtime-main.b45ce506.js"
+  }
+]);
